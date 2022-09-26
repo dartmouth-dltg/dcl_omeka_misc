d4a4998ecde3dd0b0c8c2ebf27f8c65f858648c7
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-09-26 10:38:43 -0400

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# DCL Omeka Misc
+
+An Omeka-S module for to set miscellaneous configuration options and 
+other customizations for Dartmouth Library's Omeka-S instances.
---
+++ config/module.config.php
@@ -0,0 +1,56 @@
+<?php
+  namespace DclOmekaMisc;
+
+  return array(
+    // override the default thumbnail config
+    'thumbnails' => [
+      'types' => [
+          'large' => ['constraint' => 2100],
+          'medium' => ['constraint' => 800],
+          'square' => ['constraint' => 300],
+          /*
+              Custom Thumbnail Types
+              - set strategy to 'custom'
+              - set x and y constraints as array
+              - set crop (optional)
+              - example:
+                  'og_image' => [
+                      'strategy' => 'custom',
+                      'constraint' => [
+                          'constraint_x' => 1200, 
+                          'constraint_y' => 600
+                      ],
+                      'options' => [
+                          'gravity' => 'center', 
+                          'crop' => true
+                      ]
+                  ] 
+          */
+          '16x9' => [
+              'strategy' => 'custom',
+              'constraint' => [
+                  'constraint_x' => 1440, 
+                  'constraint_y' => 810
+              ],
+              'options' => [
+                  'gravity' => 'center', 
+                  'crop' => true
+              ]
+          ],
+          'og_image' => [
+              'strategy' => 'custom',
+              'constraint' => [
+                  'constraint_x' => 1200, 
+                  'constraint_y' => 630
+              ],
+              'options' => [
+                  'gravity' => 'center', 
+                  'crop' => true
+              ]
+          ]
+      ],
+      'thumbnailer_options' => [
+          'imagemagick_dir' => null,
+      ],
+    ]
+  );
