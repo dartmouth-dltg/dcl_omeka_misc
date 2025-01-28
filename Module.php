5f295ebd9c3bd010eb0e940f51106c75a13cdc23
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2025-01-28 11:32:04 -0500

    force config load

--- Module.php
+++ Module.php
@@ -7,5 +7,10 @@ use Omeka\Module\AbstractModule;
 class Module extends AbstractModule
 {
   const NAMESPACE = __NAMESPACE__;
+  
+  public function getConfig()
 
+  {
+      return include __DIR__ . '/config/module.config.php';
+  }
 }
